41cac35aaaeb3b48f47aa69f136bccd9b585817f
vanyauhalin <vanyauhalin@gmail.com>
2021-11-04 21:28:41 +0300

    Creates select component

---
+++ wss/www/src/components/select/select.tsx
@@ -0,0 +1,39 @@
+import { ChevronDownIcon } from '@heroicons/react/outline';
+import React from 'react';
+import './select.css';
+
+export default ({
+  defaultValue,
+  legend,
+  onChange,
+  options,
+}: {
+  defaultValue: string,
+  legend: string,
+  onChange: (ev: React.BaseSyntheticEvent) => void,
+  options: {
+    id: string,
+    text: string,
+  }[],
+}) => (
+  <fieldset className="select">
+    <legend className="select__legend">
+      {legend}
+    </legend>
+    <select
+      className="select__select"
+      value={defaultValue}
+      onChange={onChange}
+    >
+      {options.map((option) => (
+        <option
+          key={option.id}
+          value={option.id}
+        >
+          {option.text}
+        </option>
+      ))}
+    </select>
+    <ChevronDownIcon className="select__icon" />
+  </fieldset>
+);
