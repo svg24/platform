dcccf6185953ffddc3ecf657e27429cb4cffc90e
vanyauhalin <vanyauhalin@gmail.com>
2021-11-06 11:04:56 +0300

    Adds src alias

--- wss/www/vite.config.ts
+++ wss/www/vite.config.ts
@@ -1,5 +1,5 @@
-import react from '@vitejs/plugin-react';
 import path from 'path';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@svg24/www': path.resolve(__dirname),
+      src: path.resolve(__dirname, 'src'),
     },
   },
   server: {
