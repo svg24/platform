47a5aef46b3198f4e20726f2402cd14dffa58ad3
vanyauhalin <vanyauhalin@gmail.com>
2021-11-18 01:20:25 +0300

    Remove unnecessary helper

--- wss/www/src/modules/layout/store/nav.ts
+++ wss/www/src/modules/layout/store/nav.ts
@@ -1,6 +1,32 @@
+import {
+  action,
+  computed,
+  makeObservable,
+  observable,
+} from 'mobx';
 import type { LayoutStore } from '../types';
-import { initElementTransitions } from './helpers/transitions';
 
 export const initNav = function (this: LayoutStore): void {
-  initElementTransitions.call(this, 'nav');
+  this.nav = {
+    _isVisible: false,
+    get isVisible() {
+      return this._isVisible;
+    },
+    set isVisible(val: boolean) {
+      this._isVisible = val;
+    },
+    show() {
+      this.isVisible = true;
+    },
+    hide() {
+      this.isVisible = false;
+    },
+  };
+
+  makeObservable(this.nav, {
+    _isVisible: observable,
+    isVisible: computed,
+    show: action,
+    hide: action,
+  });
 };
