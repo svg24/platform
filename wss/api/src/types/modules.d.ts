e5773bcbfa759acd65787b8ee9221e873bdcf647
vanyauhalin <vanyauhalin@gmail.com>
2021-11-24 16:08:57 +0300

    Added semicolons

--- wss/api/src/types/modules.d.ts
+++ wss/api/src/types/modules.d.ts
@@ -1,12 +1,12 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    API_HOST: string,
-    API_PORT: string,
-    DB_COLLECTION: string,
-    DB_NAME: string,
-    DB_PASS: string,
-    DB_PORT: string,
-    DB_USER: string,
-    NODE_ENV: string,
+    API_HOST: string;
+    API_PORT: string;
+    DB_COLLECTION: string;
+    DB_NAME: string;
+    DB_PASS: string;
+    DB_PORT: string;
+    DB_USER: string;
+    NODE_ENV: string;
   }
 }
