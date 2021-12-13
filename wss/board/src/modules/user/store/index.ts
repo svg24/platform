28629471cc0f96dd689de263cd45173da5fc91f1
vanyauhalin <vanyauhalin@gmail.com>
2021-12-13 23:42:59 +0300

    Added document property

--- wss/board/src/modules/user/store/index.ts
+++ wss/board/src/modules/user/store/index.ts
@@ -5,4 +5,13 @@ export const UserStore = new (function (this: Store) {
   initStore.call(this);
 
   this.multiplier = undefined;
+  this.document = {
+    _fontSize: parseInt(
+      window.getComputedStyle(document.documentElement).fontSize,
+      10,
+    ),
+    toRem(value) {
+      return value * this._fontSize;
+    },
+  };
 } as any as { new (): Store })();
--- wss/board/types/user.d.ts
+++ wss/board/types/user.d.ts
@@ -1,5 +1,11 @@
 import type { Store } from 'types/store';
 
 export interface UserStore extends Store<UserStore> {
+  document: UserStoreDocument;
   multiplier: number | undefined;
 }
+
+type UserStoreDocument = {
+  _fontSize: number;
+  toRem: (value: number) => number;
+};
