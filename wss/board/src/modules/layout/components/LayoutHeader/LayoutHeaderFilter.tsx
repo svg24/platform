f65b61a6e2b11cbc0f30f010dd67e3ef93f7db1a
vanyauhalin <vanyauhalin@gmail.com>
2021-12-17 02:15:25 +0300

    Refactored component

--- wss/board/src/modules/layout/components/LayoutHeader/LayoutHeaderFilter.tsx
+++ wss/board/src/modules/layout/components/LayoutHeader/LayoutHeaderFilter.tsx
@@ -1,32 +1,25 @@
 import { AdjustmentsIcon, XIcon } from '@heroicons/react/outline';
 import { reaction } from 'mobx';
 import { observer } from 'mobx-react-lite';
+import type { RefObject } from 'react';
 import { useEffect, useRef } from 'react';
 import { useStore } from 'src/store';
 import { deepAssign, getStateAnimation } from 'src/utils';
 import { LayoutHeaderButton } from './LayoutHeaderButton';
 import { LayoutHeaderIcon } from './LayoutHeaderIcon';
 
-export function LayoutHeaderFilter(): JSX.Element {
+function LayoutHeaderFilterCounter({
+  rootRef,
+}: {
+  rootRef: RefObject<HTMLDivElement>;
+}): JSX.Element {
   const { content, filter, layout } = useStore();
-
-  const root = {
-    ref: useRef<HTMLDivElement>(null),
-  };
-
+  const LayoutHeaderFilterCounterObserved = observer(() => (
+    <span className="layout-header__counter">
+      {filter.applied.length}
+    </span>
+  ));
   const counter = deepAssign({
-    el: observer(() => (
-      <span className="layout-header__counter">
-        {filter.applied.length}
-      </span>
-    )),
-    mount() {
-      useEffect(() => {
-        reaction(() => (
-          filter.applied.length && layout.main.filter.isVisible
-        ), counter.toggle);
-      }, []);
-    },
     toggle() {
       if (!filter.applied.length || layout.main.filter.isVisible) {
         if (counter.isDisplay) {
@@ -43,28 +36,39 @@ export function LayoutHeaderFilter(): JSX.Element {
       filter.reset();
       content.list.reset();
     },
-  }, getStateAnimation(root.ref, 'layout-header__filter_counter'));
+  }, getStateAnimation(rootRef, 'layout-header__filter_counter'));
 
-  counter.mount();
+  useEffect(() => {
+    reaction(() => (
+      filter.applied.length && layout.main.filter.isVisible
+    ), counter.toggle);
+  }, []);
+
+  return counter.isDisplay
+    ? (
+      <>
+        <LayoutHeaderFilterCounterObserved />
+        <LayoutHeaderButton onClick={counter.reset}>
+          <LayoutHeaderIcon icon={XIcon} />
+        </LayoutHeaderButton>
+      </>
+    )
+    : <></>;
+}
+
+export function LayoutHeaderFilter(): JSX.Element {
+  const { layout } = useStore();
+  const ref = useRef<HTMLDivElement>(null);
 
   return (
     <div
       className="layout-header__filter"
-      ref={root.ref}
+      ref={ref}
     >
       <LayoutHeaderButton onClick={layout.main.filter.toggle}>
         <LayoutHeaderIcon icon={AdjustmentsIcon} />
       </LayoutHeaderButton>
-      {counter.isDisplay
-        ? (
-          <>
-            <counter.el />
-            <LayoutHeaderButton onClick={counter.reset}>
-              <LayoutHeaderIcon icon={XIcon} />
-            </LayoutHeaderButton>
-          </>
-        )
-        : <></>}
+      <LayoutHeaderFilterCounter rootRef={ref} />
     </div>
   );
 }
