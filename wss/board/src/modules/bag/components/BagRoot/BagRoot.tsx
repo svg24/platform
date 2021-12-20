f7d85e1080bba265b5f3b4b07f1f9514cd726d5d
vanyauhalin <vanyauhalin@gmail.com>
2021-12-21 01:37:59 +0300

    Moved transition into root component

--- wss/board/src/modules/bag/components/BagRoot/BagRoot.tsx
+++ wss/board/src/modules/bag/components/BagRoot/BagRoot.tsx
@@ -1,43 +1,57 @@
-import { forwardRef, useEffect, useRef } from 'react';
+import { reaction } from 'mobx';
+import { useEffect, useRef, useState } from 'react';
+import { Transition } from 'src/components';
 import { useStore } from 'src/store';
-import { useForkRef } from 'src/utils/useForkRef';
 
-export const BagRoot = (
-  forwardRef<HTMLDivElement, { children: JSX.Element[] }>((props, ref) => {
-    const { bag, layout } = useStore();
-    const localRef = useRef<HTMLDivElement>(null);
-    const foreignRef = useForkRef(localRef, ref);
+function BagRoot({ children }: { children: JSX.Element[] }): JSX.Element {
+  const { bag, layout } = useStore();
+  const rootRef = useRef<HTMLDivElement>(null);
+  const [bagIsVisible, setBagIsVisible] = useState(false);
 
-    function handleClose(): void {
-      layout.bag.hide();
-      if (layout.bag.goBack) layout.bag.goBack();
+  function handleClose(): void {
+    layout.bag.hide();
+    if (layout.bag.goBack) layout.bag.goBack();
+  }
+  function handleClick(ev: MouseEvent): void {
+    if (ev.target === rootRef.current) handleClose();
+  }
+  function handleFocusout(ev: FocusEvent): void {
+    if (!ev.relatedTarget) return;
+    if (!rootRef.current?.contains(ev.relatedTarget as Node)) handleClose();
+  }
+
+  useEffect(() => {
+    rootRef.current?.focus();
+    rootRef.current?.addEventListener('click', handleClick);
+    rootRef.current?.addEventListener('focusout', handleFocusout);
+  }, [bagIsVisible]);
+
+  reaction(() => layout.bag.isVisible, () => {
+    if (layout.bag.isVisible) {
+      setBagIsVisible(true);
+    } else {
+      setBagIsVisible(false);
     }
-    function handleClick(ev: MouseEvent): void {
-      if (ev.target === localRef.current) handleClose();
-    }
-    function handleFocusout(ev: FocusEvent): void {
-      if (!ev.relatedTarget) return;
-      if (!localRef.current?.contains(ev.relatedTarget as Node)) handleClose();
-    }
+  });
 
-    useEffect(() => {
-      localRef.current?.focus();
-      localRef.current?.addEventListener('click', handleClick);
-      localRef.current?.addEventListener('focusout', handleFocusout);
-    }, []);
-
-    return (
+  return (
+    <Transition
+      classNames="bag-root"
+      isVisible={bagIsVisible}
+    >
       <div
         aria-label={`${bag.item.meta?.name} details`}
         className="bag-root"
-        ref={foreignRef}
+        ref={rootRef}
         role="presentation"
         tabIndex={-1}
       >
         <div className="bag-root__inner">
-          {props.children}
+          {children}
         </div>
       </div>
-    );
-  })
-);
+    </Transition>
+  );
+}
+
+export { BagRoot };
--- wss/board/src/modules/bag/index.tsx
+++ wss/board/src/modules/bag/index.tsx
@@ -1,7 +1,4 @@
-import { reaction } from 'mobx';
-import { useState } from 'react';
-import { Portal, Transition } from 'src/components';
-import { useStore } from 'src/store';
+import { Portal } from 'src/components';
 import { BagContent } from './components/BagContent';
 import { BagHeading } from './components/BagHeading';
 import { BagMeta } from './components/BagMeta';
@@ -11,31 +8,15 @@ import { BagView } from './components/BagView';
 import { BagStore } from './store';
 
 function Bag(): JSX.Element {
-  const { layout } = useStore();
-  const [bagIsVisible, setBagIsVisible] = useState(false);
-
-  reaction(() => layout.bag.isVisible, () => {
-    if (layout.bag.isVisible) {
-      setBagIsVisible(true);
-    } else {
-      setBagIsVisible(false);
-    }
-  });
-
   return (
     <Portal>
-      <Transition
-        classNames="bag-root"
-        isVisible={bagIsVisible}
-      >
-        <BagRoot>
-          <BagHeading />
-          <BagMeta />
-          <BagView />
-          <BagSettings />
-          <BagContent />
-        </BagRoot>
-      </Transition>
+      <BagRoot>
+        <BagHeading />
+        <BagMeta />
+        <BagView />
+        <BagSettings />
+        <BagContent />
+      </BagRoot>
     </Portal>
   );
 }
