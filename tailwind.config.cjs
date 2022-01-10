3b9fcb713c2cc57e5dde9ac975d23447ea45b114
vanyauhalin <vanyauhalin@gmail.com>
2022-01-10 08:54:26 +0300

    Added colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,20 @@ module.exports = {
     colors: {
       transparent: 'var(--color-transparent)',
       white: 'var(--color-white)',
+      'white-a70': 'var(--color-white-a70)',
       black: 'var(--color-black)',
+      accent: {
+        50: 'var(--color-accent-50)',
+        100: 'var(--color-accent-100)',
+        200: 'var(--color-accent-200)',
+        300: 'var(--color-accent-300)',
+        400: 'var(--color-accent-400)',
+        500: 'var(--color-accent-500)',
+        600: 'var(--color-accent-600)',
+        700: 'var(--color-accent-700)',
+        800: 'var(--color-accent-800)',
+        900: 'var(--color-accent-900)',
+      },
       negative: {
         50: 'var(--color-negative-50)',
         100: 'var(--color-negative-100)',
