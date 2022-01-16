fd2dd14e2eb5db5575b3e6d9c48d666a8137386c
vanyauhalin <vanyauhalin@gmail.com>
2022-01-16 06:43:40 +0300

    Added mime type for webp

--- packages/assets/src/constants.ts
+++ packages/assets/src/constants.ts
@@ -2,5 +2,6 @@ export const MIME_TYPES: { [key: string]: string } = {
   '.css': 'text/css',
   '.ico': 'image/x-icon',
   '.svg': 'image/svg+xml',
+  '.webp': 'image/webp',
   '.woff2': 'application/font-woff2',
 };
